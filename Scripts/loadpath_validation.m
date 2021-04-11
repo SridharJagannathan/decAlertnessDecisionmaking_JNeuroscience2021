3c51e2da8ebb24d923100757e85206aba6462e18
Sridhar Jagannathan <j.sridharrajan@gmail.com>
2021-04-11 15:28:59 +0100

    Updated first pass

---
+++ Scripts/loadpath_validation.m
@@ -0,0 +1,24 @@
+%% Set the path for the external toolbox folder and add it to MATLAB search directory
+
+%% Determine location
+global pathappend
+ pathappend = '/rds/project/tb419/rds-tb419-bekinschtein/Sri/';
+
+%% Add paths now..
+%SPM path
+spm_toolbox = [pathappend 'SpatialAttention_Drowsiness/Scripts/toolboxes/spm12'];
+addpath(spm_toolbox);
+
+
+%Fieldtrip path
+ftp_toolbox = [pathappend 'SpatialAttention_Drowsiness/Scripts/toolboxes/fieldtrip-20151223'];
+addpath(ftp_toolbox);
+%ft_defaults;
+
+%EEGlab path
+eeglab_toolbox = [pathappend 'SpatialAttention_Drowsiness/Scripts/toolboxes/eeglab13_5_4b'];
+addpath(genpath(eeglab_toolbox));
+
+%plot tools path
+plot_toolbox = [pathappend 'SpatialAttention_Drowsiness/decArsousalDecisionmaking_JNeuroscience2021/Scripts/plot_tools'];
+addpath(genpath(plot_toolbox));
